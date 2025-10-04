d948a903def2c3fce826243e95834a3e9c79cc5b
neerajsinghnegi <neerajsinghnegi@mac.local>
2025-10-04 22:40:33 +0530

    added helm and infra workflow

---
+++ terraform/main.tf
@@ -0,0 +1,96 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_iam_policy_document" "ebs_csi_assume" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [module.eks.oidc_provider_arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${module.eks.oidc_provider}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ebs_csi_driver" {
+  name               = "${module.eks.cluster_name}-ebs-csi-driver"
+  assume_role_policy = data.aws_iam_policy_document.ebs_csi_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_policy" {
+  role       = aws_iam_role.ebs_csi_driver.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = "democluster"
+  kubernetes_version = "1.33"
+  region = "ap-south-1"
+
+
+  addons = {
+    aws-ebs-csi-driver = {
+      most_recent = true
+      resolve_conflicts = "OVERWRITE"
+      service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
+    }
+    coredns                = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy             = {}
+    vpc-cni                = {
+      before_compute = true
+    }
+  }
+
+  # Optional
+  endpoint_public_access = true
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+  enable_irsa = true
+  vpc_id     = data.aws_vpc.default.id
+  subnet_ids = data.aws_subnets.default.ids
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
+    demonodepool = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["c7i-flex.large"]
+
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+    }
+  }
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+terraform {
+  backend "aws" {
+    bucket  = "tf-devsecops-statefile"
+    prefix  = "state"
+  }
+}
