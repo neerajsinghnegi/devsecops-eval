1d2a7c96237381d50bd71b020a21724c39c6baff
neerajsinghnegi <neerajsinghnegi@mac.local>
2025-10-04 23:11:25 +0530

    added helm and infra workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -90,7 +90,15 @@ module "eks" {
 
 terraform {
   backend "aws" {
-    bucket  = "tf-devsecops-statefile"
+    bucket  = ""
     prefix  = "state"
+  }
+}
+terraform {
+  backend "s3" {
+    bucket         = "tf-devsecops-statefile"
+    key            = "eks/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
   }
 }
